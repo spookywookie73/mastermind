776d99a2fa72be5b4afee754aa1a1b9d422fcee4
Reece Bradley <naruto73@hotmail.co.uk>
2023-02-02 16:49:59 +0000

    add code to check for numbers

--- mastermind.rb
+++ mastermind.rb
@@ -10,7 +10,8 @@ class Mastermind
 
   def rules
     puts <<-TEXT
-      \nIn this game you have 12 turns to guess a 4 digit code.
+      \n
+      In this game you have 12 turns to guess a 4 digit code.
       The numbers will range from 1 to 6 and can appear more than once.
       After every guess you will be given clues. The clues will consist
       of an X or an O. The X will mean that 1 number is correct and is in 
@@ -29,14 +30,15 @@ class Mastermind
         puts "Game Over!"
         break
       elsif round == 12
-        puts "This is your final guess. Choose wisely."
-        input = gets.chomp.to_i
+        print "This is your final guess. Choose wisely: "
+        input = guess(gets.chomp).to_i
           if input == code
             puts "You win. The code was #{code}."
             break
           end
       else
-        input = gets.chomp.to_i
+        print "Enter your guess: "
+        input = guess(gets.chomp).to_i
         if input == code
           puts "You win. The code was #{code}."
           break
@@ -45,9 +47,18 @@ class Mastermind
       @round += 1
     end
   end
+
+  def guess(input)
+    if (input.to_i != 0) && (input.to_s.length == 4)
+      input
+    else
+      print "Please enter 4 valid numbers: "
+      input = guess(gets.chomp).to_i
+    end
+  end 
   
   def computer_code
-    [rand(1..6)].join.to_i
+    [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].join.to_i
   end
 
 end
